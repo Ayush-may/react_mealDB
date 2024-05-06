8d4ac06298785a35141bbe99ad6db536666113d8
Ayush-may <Ayushsharma014may@gmail.com>
2024-05-07 01:57:36 +0530

    Refactor: removed code and add code

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import BigImage from "./components/BigImage";
 import CategoryContainer from "./components/CategoryContainer";
+import ItemsContainer from "./components/ItemsContainer";
 
 const App = () => {
   return (
@@ -9,14 +10,7 @@ const App = () => {
       <NavBar />
       <BigImage />
       <CategoryContainer />
-      <div className="lg:px-40 md:px-28 w-full h-full">
-        <div className="w-full h-full lg:p-0 sm:p-3 rounded-md">
-          <h1 className="lg:text-5xl text-3xl mb-5 lg:mt-10 mt-4 px-2">
-            Items
-          </h1>
-          <div className="items_area"></div>
-        </div>
-      </div>
+      <ItemsContainer />
     </>
   );
 };
